ad4705802c2a9117b3d8160e9ca71707845685d3
RobertMahin <113948918+RobertMahin@users.noreply.github.com>
2023-02-13 08:26:02 -0800

    Delete SortingInterface.java

--- src/SortingInterface.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface SortingInterface {
-
-    // logs list of sorted products before returning (prints to console)
-    List<Product> improvedSort(List<Product> items, int sortingApproach);
-}
