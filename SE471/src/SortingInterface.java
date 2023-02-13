9ca00cec3ede1a2fd4f9780fddef757496cb7d04
RobertMahin <113948918+RobertMahin@users.noreply.github.com>
2023-02-13 08:20:14 -0800

    Add files via upload

---
+++ SE471/src/SortingInterface.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface SortingInterface {
+
+    // logs list of sorted products before returning (prints to console)
+    List<Product> improvedSort(List<Product> items, int sortingApproach);
+}
